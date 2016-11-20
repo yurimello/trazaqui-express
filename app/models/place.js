9d08e0d8a07fc6c9752610b30643c0757218dfd4
Yuri Mello <yuri.mcr@gmail.com>
2016-11-20 19:19:19 -0200

    add controller

--- app/models/place.js
+++ app/models/place.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
 
+const PlaceSchema = new Schema({
+  name: String
+});
+PlaceSchema.pre('save', function(next) {
+  console.log('saving');
+  next();
+})
+module.exports = mongoose.model('Place', PlaceSchema);
--- server.js
+++ server.js
@@ -8,6 +8,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dbConnection = mongoose.connect('mongodb://trazaqui:master01@ds159237.mlab.com:59237/trazaqui-express').connection;
+// const connection = mongoose.connect('mongodb://localhost/test').connection;
+
+dbConnection.on('error', console.log)
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -17,23 +23,28 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
-// ROUTES FOR OUR API
+// ROUTES FOR API
 // =============================================================================
 
-var router = express.Router();     // get an instance of express router
+// middleware to use for all requests
+app._router.use(function(req, res, next){
+  // do loggin
+  console.log('Request get');
+  next();
+})
 
-// test route to make sure everything is working (accessed at GET http://localhost:3000/api)
-
-router.get('/', function(req, res){
+app._router.get('/', function(req, res){
   res.json({message: 'Express Hello'});
 })
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /
-
-app.use('/api', router);
+// CONTROLLERS FOR API
+// =============================================================================
+fs.readdirSync('./app/controllers').forEach(function (file) {
+  if(file.substr(-3) == '.js') {
+    route = require('./app/controllers/' + file);
+    route.controller(app);
+  }
+});
 
 // START THE SERVER
 // =============================================================================
