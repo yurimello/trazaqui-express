bf1b9d4e3ca368c54863fdfdfbae7001792a2097
Yuri Mello <yuri.mcr@gmail.com>
2016-11-20 22:13:29 -0200

    using localhost mongodb for development environment

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   "db": {
-    "development": "mongodb://trazaqui:master01@ds159237.mlab.com:59237/trazaqui-express",
+    "production":  "mongodb://trazaqui:master01@ds159237.mlab.com:59237/trazaqui-express",
     "test":        "mongodb://localhost/trazaqui_express_test",
+    "development": "mongodb://localhost/trazaqui_express_development"
   },
 };
