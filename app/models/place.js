9c70a57c6a161a29e72b014edd0093be41cb977d
Yuri Mello <yuri.mcr@gmail.com>
2016-11-27 16:03:08 -0200

    Add items schema to place model

--- app/models/place.js
+++ app/models/place.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
+const ItemsSchema = new Schema({
+  name: { type: String, required: true },
+})
+
 const PlaceSchema = new Schema({
   name: { type: String, required: true },
+  items: [ ItemsSchema ],
 });
 
 module.exports = mongoose.model('Place', PlaceSchema);
